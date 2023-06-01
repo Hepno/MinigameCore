d780195f25a8f88e67bae4d64798d7a58ffb079d
hepno <me@hepno.dev>
2023-06-01 22:19:38 +0200

    chore: add comments

--- src/main/java/dev/hepno/minigamecore/MinigameCore.java
+++ src/main/java/dev/hepno/minigamecore/MinigameCore.java
@@ -8,6 +8,18 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MinigameCore extends JavaPlugin {
 
+    /*
+    Welcome to the MinigameCore source code!
+    This plugin is meant to be a framework for creating minigames, I made it because
+    after rewriting this exact code for the 30th time, I decided it was time to make
+    a framework for it.
+
+    This was made for me, and the target audience is myself, but if you want to use it,
+    go ahead! I don't mind at all, but support will be limited.
+
+    Credit to Stephen King for writing a large portion of the code.
+     */
+
     private ArenaManager arenaManager;
 
     @Override
